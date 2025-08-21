0432340fd78f40f5659e600e0931154a5e092d64
gaby994 <anagabrielagauna@hotmail.com>
2025-08-21 11:56:12 -0600

    semana 3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Posgrado_Estadistica_2025
 Material del curso de posgrado de Experimentación y métodos Estadisticos en MCF Y DCMN
 semana 1
+Jueves
--- importar datos.R
+++ importar datos.R
@@ -1,3 +1,8 @@
+#Ana Gabriela Gauna Rdz
+#21/08/25
+#semana 3
+#Importar datos de temperatura
+
 temperatura <- read.csv("C:/Users/Usuario/Desktop/temperatura.csv")
 View(temperatura)
 
@@ -9,5 +14,29 @@ str(temperatura) #Estructura del data frame
 summary(temperatura) #Resumen estadistico
 names(temperatura) <- c("Anual","Ene","Feb","Mar","Abr", "May","Jun","Jul","Ago","Sep","Oct","Nov","Dic")#Corregir nombre de columna
 
-temperatura$media_anual <- rowMeans(temperatura[11:21,])
-  
+write.csv(temperatura, "temp_final.csv")
+temperatura$media_anual <- rowMeans(temperatura[11:21])
+temp <- (temperatura[, 2:13])
+temp10 <- temperatura[11:21, 2:13]
+temperatura[2,2]
+colores <- c("pink","slateblue","blue")
+
+#Crear un bloxplot con las temperaturas de 10 años 2010 a 2020
+boxplot(temp, col = colores,
+        main ="Comportamiento temperatura (2000 a 2020)",
+        xlab ="Meses",
+        ylab ="Temperatura (c)"
+        )
+
+
+
+
+# Importar datos web ------------------------------------------------------
+
+url <-("https://repodatos.atdt.gob.mx/api_update/",
+       "senasica/actividades_inspeccion_movilizacion/",
+       "29_actividades-inspeccion-movilizacion.csv")
+senasica <- read.csv(url,header=T)
+
+
+
